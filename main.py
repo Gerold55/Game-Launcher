4392cc869251a197150ef90aa5b5afd60adb31f5
Gerold55 <marleybeare56@gmail.com>
2020-03-16 19:43:27 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,29 +2,25 @@ from tkinter import *
 
 
 
-def createWindow():
-	window = Tk()
-	
-	# add widget here
-	b = Button(window, text="News")
-	b = Button(window, text="ModStore")
-	b = Button(window, text="Games/Modpacks/Saves")
-	b = Button(window, text="Worlds")
-	b = Button(window, text="Servers")
-	b.pack()
-	
-	# add title
-	window.title("Phoenix Launcher")
-	# set frame & geometry (widthxheight+XPOS+YPOS)
-	window.geometry("1000x600+100+200")
-	window.mainloop()
-	
+def create_window():
+    window = Tk()
+
+def button_callback():
+    print('Hello World')
 
 if __name__ == '__main__':
-	createWindow()
+    window = create_window()
+    # Rest of code goes here
 	
 	
 	
 	
 	
+b = Button(window, text="News", command=button_callback)
+b.pack()
 	
+	# add title
+window.title("Phoenix Launcher")
+	# set frame & geometry (widthxheight+XPOS+YPOS)
+window.geometry("1000x600+100+200")
+window.mainloop()
